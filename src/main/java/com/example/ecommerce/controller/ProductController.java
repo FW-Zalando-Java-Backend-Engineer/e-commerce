7c03aef9fec6efa0ca8be44bd5275bdc2b053480
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-07-09 12:47:14 +0200

    adding the Controller layer

--- src/main/java/com/example/ecommerce/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -1,4 +1,65 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.model.Product;
+import com.example.ecommerce.service.ProductService;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * REST Controller for mapping HTTP'S Requests' URL's with Java methods.
+ * Used for managing products.
+ * Talks to Web Client and Service layer.
+ * */
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+
+    /**
+     * Constructor injection of service.
+     * @param productService service bean.
+     * */
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    /**
+     * Create a new product.
+     * POST /api/products – to add new products
+     * @param product product to create
+     * @return inserted product
+     */
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.addProduct(product);
+    }
+
+    /**
+     * Get all products.
+     * GET /api/products – to list all
+     * @return list of products
+     */
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    /**
+     * Get a product by ID.
+     * GET /api/products/{id} – to find by ID
+     * @param id product ID
+     * @return optional product
+     */
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable String id){
+        return productService.getProductById(id);
+    }
+
 }
